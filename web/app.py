b0b56acd0bda8df6944f1bb2e05d4cb1c210e63b
riadhassan <riad_hassan@outlook.com>
2019-05-17 12:57:32 +0600

    configure web app with web template

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template, request
+from bottle import route, run, template, request,view
 
 from card_validator.validator import get_issuer
 
@@ -8,20 +8,30 @@ def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 @route('/')
+@view('index')
 def test():
-    return "Hello world"
+    return {
+        "message": "Hello world"
+    }
 
 @route('/validate')
+@view('validate')
 def validate():
     cardNumber = request.query.get("cardNumber","").strip()
     if cardNumber:
         try:
             issuer = get_issuer(cardNumber)
-            return "Issuer: {}".format(issuer)
+            return {
+               "cardNumber" : cardNumber, "issuer" : issuer, "message": "It is a {}".format(issuer)
+            }
 
         except ValueError:
-            return "Your card number is invalid"
+            return {
+               "cardNumber" : "n/a", "issuer" : "n/a", "message": "Card number is invalid"
+            }
 
-    return "I am card validator page"
+    return {
+         "cardNumber" : "n/a", "issuer" : "n/a","message" : "welcome to card validator"
+    }
 
 run(host='localhost', port=8080)
